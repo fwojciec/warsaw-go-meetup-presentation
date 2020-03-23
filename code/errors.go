9b703539a2bd2e5c676172f45cf1fa26e193c1f0
Filip Wojciechowski <fwojciec@gmail.com>
2020-03-23 17:32:34 +0100

    initial commit

---
+++ code/errors.go
@@ -0,0 +1,20 @@
+package main
+
+import "errors"
+
+// START OMIT
+var (
+	// ErrInvalid is returned when a params or input values are invalid.
+	ErrInvalid = errors.New("invalid")
+
+	// ErrNotFound is returned when a queried resource was not found.
+	ErrNotFound = errors.New("not found")
+
+	// ErrPwdCheck is returned when password verification has failed.
+	ErrPwdCheck = errors.New("password check failed")
+
+	// ErrUnauthorized is return on unauthorized access attempt.
+	ErrUnauthorized = errors.New("unauthorized")
+)
+
+// END OMIT
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/fwojciec/meetuppresentation
+
+go 1.14
+
+require (
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
+	golang.org/x/net v0.0.0-20200320220750-118fecf932d8 // indirect
+	golang.org/x/tools v0.0.0-20200321014904-268ba720d32c // indirect
+)
